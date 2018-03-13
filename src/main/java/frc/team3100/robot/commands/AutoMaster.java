97e450bc27536384d0b76e86fc2fb8166c59e4bc
aiden7301 <aidenaaj@gmail.com>
2018-03-13 16:46:22 -0500

    changes thorugh lake superior

---
+++ src/main/java/frc/team3100/robot/commands/AutoMaster.java
@@ -0,0 +1,47 @@
+package frc.team3100.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team3100.robot.Robot;
+
+
+public class AutoCubeScore extends Command {
+    private int time = 0;
+    public AutoCubeScore() {
+        super("AutoCubeScore");
+        requires(Robot.claw);
+
+    }
+
+
+    protected void initialize() {
+
+    }
+
+    protected void execute() {
+        Robot.claw.score();
+        time += 1;
+
+
+    }
+
+
+    protected boolean isFinished() {
+        if(time > 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    protected void interrupted(){
+
+    }
+
+
+    protected void end() {
+        Robot.claw.stop();
+
+    }
+}
+
